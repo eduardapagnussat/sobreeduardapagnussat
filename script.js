8639896b822f715f2b736a665b344c27ab22cb51
eduardapagnussat <dudafpagnussat@gmail.com>
2024-09-26 13:42:22 -0300

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//document.addEventListener("DOMContentLoaded", function () {
+  //  const nome = document.getElementById("nome");
+
+    // Iniciar animação de escrita
+    //setTimeout(() => {
+      //  nome.style.width = nome.textContent.length + 'ch';
+    //}, 30);
+//});
